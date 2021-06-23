1b9892837204fb26fba26bb2e344d38a1ba62c78
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-06-23 16:22:17 +0300

    save file full bill with all items updated

--- bill.go
+++ bill.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 //create type for bill
@@ -46,4 +50,77 @@ func (b *bill) updateTip(tip float64) {
 func (b *bill) addItem(name string, price float64) {
 	b.items[name] = price
 
+}
+
+// creat a function to get users input
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	//print something to the console
+	fmt.Println(prompt)
+
+	//get the input from console
+	input, err := r.ReadString('\n')
+
+	//return the output of the function
+	return strings.TrimSpace(input), err
+
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("Create a new bill name:")
+	//name, _ := reader.ReadString('\n')
+	name, _ := getInput("Create a new bill name:", reader)
+	fmt.Sprintf("The new bill has been created with the name: %v \n", name)
+	b := newBill(name)
+	return b
+}
+func (b *bill) save() {
+	data := []byte(b.format())
+	err := os.WriteFile("bills_"+b.name+".txt", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("the bill saved")
+}
+
+func promptOptions(b bill) {
+	//get the input from the console
+	reader := bufio.NewReader(os.Stdin)
+
+	//print the prompt to the console
+	opt, _ := getInput("choose an option (a - add item, s - save bill, t - add tip)", reader)
+	switch opt {
+	case "a":
+		name, _ := getInput("enter the item:", reader)
+		price, _ := getInput("enter the price:", reader)
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("try again")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+
+		fmt.Println(name, price)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("enter the tip amount ($):", reader)
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("try again")
+			promptOptions(b)
+		}
+
+		b.updateTip(t)
+		fmt.Println(tip)
+		promptOptions(b)
+	case "s":
+		b.save()
+		fmt.Println("you bill saved", b.name)
+		fmt.Println(b.format())
+	default:
+		fmt.Println("this is was not a valid option")
+		promptOptions(b)
+
+	}
 }
--- go_struct.go
+++ go_struct.go
@@ -1,17 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
+	mybill := createBill()
+	promptOptions(mybill)
 
-	//TODO create a bill structure inside bill file
-	//call this bill here
-
-	mybill := newBill("ahmed Yaseen")
-	mybill.updateTip(50.5254)
-	mybill.addItem("anber rise", 55.7584)
-	mybill.addItem("egg", 25.365)
-
-	fmt.Println(mybill.format())
+	//fmt.Println(mybill.format())
 	//type bill
 }
